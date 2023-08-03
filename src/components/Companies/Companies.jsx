458065744ff23ca8e32eb2f41c2f97ce80208968
NoOne <abdul.hannan13300@gmail.com>
2023-08-03 09:20:45 +0530

    .

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Companies.css";
 
 const Companies = () => {
-  return <div>Companies</div>;
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="prologis logo" />
+        <img src="./tower.png" alt="tower logo" />
+        <img src="./equinix.png" alt="equinix logo" />
+        <img src="./realty.png" alt="realty logo" />
+      </div>
+    </section>
+  );
 };
 
 export default Companies;
---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "./Residencies.css";
+import "../../utils/slider.json";
+
+const Residencies = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular Residencies</span>
+        </div>
+
+        <Swiper>
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="r-card">
+                <img src={card.image} alt="home" />
+                <span className="secondaryText r-price">
+                  <span style={{ color: "orange" }}>$</span>
+                  <span>{card.price}</span>
+                </span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Residencies;
